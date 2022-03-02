e4e67ab73f41028bf623ed41925120023d3c02e3
nabeomki <beomki69@gmail.com>
2022-03-02 23:11:35 +0900

    feat: Add Header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  const navigate = useNavigate();
+
+  const username = "ki"; //
+
+  const onAllTweets = () => {
+    navigate("/");
+  };
+
+  const onMyTweets = () => {
+    navigate(`/${username}`);
+  };
+
+  const onLogout = () => {
+    if (window.confirm("Do you want to log out?")) {
+      // logout();
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header
+        username={username}
+        onLogout={onLogout}
+        onMyTweets={onMyTweets}
+        onAllTweets={onAllTweets}
+      />
+      <Routes>
+        <Route path="/" />
+        <Route path="/:username" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { memo } from "react";
+
+type HeaderProps = {
+  username: string;
+  onLogout: () => void;
+  onMyTweets: () => void;
+  onAllTweets: () => void;
+};
+
+const Header = memo(
+  ({ username, onLogout, onMyTweets, onAllTweets }: HeaderProps) => {
+    return (
+      <header className="header">
+        <div className="logo">
+          <img src="./img/logo.png" alt="Kwitter Logo" className="logo-img" />
+          <h1 className="logo-name">Dwitter</h1>
+          {username && <span className="logo-user">@{username}</span>}
+        </div>
+        {username && (
+          <nav className="menu">
+            <button onClick={onAllTweets}>All Tweets</button>
+            <button onClick={onMyTweets}>My Tweets</button>
+            <button className="menu-item" onClick={onLogout}>
+              Logout
+            </button>
+          </nav>
+        )}
+      </header>
+    );
+  }
+);
+
+export default Header;
