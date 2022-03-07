f22cc24346ebf42f2823ed424cd151dd654e6ede
nabeomki <beomki69@gmail.com>
2022-03-08 00:41:18 +0900

    feat: Add TweetCard

---
+++ client/src/components/TweetCard.tsx
@@ -0,0 +1,63 @@
+import { memo, useState } from "react";
+import { Tweet } from "../service/tweet";
+import parseDate from "../util/date";
+import Avatar from "./Avatar";
+import EditTweetForm from "./EditTweetForm";
+
+type TweetCardProps = {
+  tweet: Tweet;
+  owner: boolean;
+  onDelete: (tweetId: number) => Promise<void>;
+  onUpdate: (tweetId: number, text: string) => Promise<any>;
+  onUsernameClick: (tweet: Tweet) => void;
+};
+
+const TweetCard = memo(
+  ({ tweet, owner, onDelete, onUpdate, onUsernameClick }: TweetCardProps) => {
+    const { id, username, name, url, text, createdAt } = tweet;
+    const [editing, setEditing] = useState(false);
+
+    const onClose = () => setEditing(false);
+
+    return (
+      <li className="tweet">
+        <section className="tweet-container">
+          <Avatar url={url} name={name} />
+          <div className="tweet-body">
+            <span className="tweet-name">{name}</span>
+            <span
+              className="tweet-username"
+              onClick={() => onUsernameClick(tweet)}
+            >
+              @{username}
+            </span>
+            <span className="tweet-date"> · {parseDate(createdAt)}</span>
+            <p>{text}</p>
+            {editing && (
+              <EditTweetForm
+                tweet={tweet}
+                onUpdate={onUpdate}
+                onClose={onClose}
+              />
+            )}
+          </div>
+        </section>
+        {owner && (
+          <div className="tweet-action">
+            <button className="tweet-action-btn" onClick={() => onDelete(id)}>
+              x
+            </button>
+            <button
+              className="tweet-action-btn"
+              onClick={() => setEditing(true)}
+            >
+              ✎
+            </button>
+          </div>
+        )}
+      </li>
+    );
+  }
+);
+
+export default TweetCard;
