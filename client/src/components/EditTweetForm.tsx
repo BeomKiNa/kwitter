8ad6adfd19540f9d039d2b93257c27279cb28dca
nabeomki <beomki69@gmail.com>
2022-03-08 00:38:27 +0900

    feat: Add EditTweetForm

---
+++ client/src/components/EditTweetForm.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Tweet } from "../service/tweet";
+
+type EditTweetFormProps = {
+  tweet: Tweet;
+  onUpdate: (tweetId: number, text: string) => Promise<any>;
+  onClose: () => void;
+};
+
+const EditTweetForm = ({ tweet, onUpdate, onClose }: EditTweetFormProps) => {
+  const [text, setText] = useState(tweet.text);
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onUpdate(tweet.id, text);
+    onClose();
+  };
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <form className="edit-tweet-form" onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Edit your tweet"
+        value={text}
+        required
+        autoFocus
+        onChange={onChange}
+        className="form-input tweet-input"
+      />
+      <div className="edit-tweet-form-action">
+        <button type="submit" className="form-btn-update">
+          Update
+        </button>
+        <button type="button" className="form-btn-cancel" onClick={onClose}>
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EditTweetForm;
