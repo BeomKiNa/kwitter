8fb27ebee9e9eab694d88bd62f7cc579dec4a021
nabeomki <beomki69@gmail.com>
2022-03-18 01:08:45 +0900

    Init Setup

---
+++ server/router/tweets.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "express-async-error";
+
+const router = express.Router();
+// GET /tweets
+// GET /tweets?username=:username
+// GET /tweets/:id
+// POST /tweets
+// PUT /tweets/:id
+// DELETE /tweets/:id
+export default router;
