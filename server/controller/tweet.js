63e0463268667c8376ee04933e9640150f5a25ff
nabeomki <beomki69@gmail.com>
2022-03-24 01:35:40 +0900

    refactor: controller 분리

---
+++ server/controller/tweet.js
@@ -0,0 +1,44 @@
+import * as tweetRepository from "../data/tweet.js";
+
+export function getTweets(req, res) {
+  const { username } = req.query;
+  const data = username
+    ? tweetRepository.getAllByUsername(username)
+    : tweetRepository.getAll();
+  res.status(200).json(data);
+}
+
+export function getTweet(req, res) {
+  const { id } = req.params;
+  const tweet = tweetRepository.getById(id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+}
+
+export function createTweet(req, res) {
+  const { text, name, username } = req.body;
+  const tweet = tweetRepository.create(text, name, username);
+  res.status(201).json(tweet);
+}
+
+export function updateTweet(req, res) {
+  const {
+    params: { id },
+    body: { text },
+  } = req;
+  const tweet = tweetRepository.update(id, text);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+}
+
+export function deleteTweet(req, res) {
+  const { id } = req.params;
+  tweetRepository.remove(id);
+  res.sendStatus(204);
+}
