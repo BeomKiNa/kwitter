59bf8968a5ee2c99c40e513c87006bfde9927f0d
nabeomki <beomki69@gmail.com>
2022-03-24 01:27:43 +0900

    refactor: data 분리

---
+++ server/data/tweet.js
@@ -0,0 +1,54 @@
+let tweets = [
+  {
+    id: "1",
+    text: "first Tweet",
+    createdAt: Date.now().toString(),
+    name: "Bob",
+    username: "bob",
+    url: "https://images.unsplash.com/photo-1646291127886-613cd73974d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    id: "2",
+    text: "second Tweet",
+    createdAt: Date.now().toString(),
+    name: "Ki",
+    username: "ki",
+    url: "https://images.unsplash.com/photo-1646291127886-613cd73974d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+];
+
+export function getAll() {
+  return tweets;
+}
+
+export function getAllByUsername(username) {
+  return tweets.filter((tweet) => tweet.username === username);
+}
+
+export function getById(id) {
+  return tweets.find((tweet) => tweet.id === id);
+}
+
+export function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export function update(id, text) {
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export function remove(id) {
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+}
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,24 +1,6 @@
-import express, { json } from "express";
+import express from "express";
 import "express-async-error";
-
-let tweets = [
-  {
-    id: "1",
-    text: "first Tweet",
-    createdAt: Date.now().toString(),
-    name: "Bob",
-    username: "bob",
-    url: "https://images.unsplash.com/photo-1646291127886-613cd73974d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-  },
-  {
-    id: "2",
-    text: "second Tweet",
-    createdAt: Date.now().toString(),
-    name: "Ki",
-    username: "ki",
-    url: "https://images.unsplash.com/photo-1646291127886-613cd73974d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-  },
-];
+import * as tweetRepository from "../data/tweet.js";
 
 const router = express.Router();
 
@@ -27,15 +9,15 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const { username } = req.query;
   const data = username
-    ? tweets.filter((tweet) => tweet.username === username)
-    : tweets;
+    ? tweetRepository.getAllByUsername(username)
+    : tweetRepository.getAll();
   res.status(200).json(data);
 });
 
 // GET /tweets/:id
 router.get("/:id", (req, res, next) => {
   const { id } = req.params;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweetRepository.getById(id);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -46,14 +28,7 @@ router.get("/:id", (req, res, next) => {
 // POST /tweets
 router.post("/", (req, res, next) => {
   const { text, name, username } = req.body;
-  const tweet = {
-    id: Date.now().toString(),
-    text,
-    createdAt: new Date(),
-    name,
-    username,
-  };
-  tweets = [tweet, ...tweets];
+  const tweet = tweetRepository.create(text, name, username);
   res.status(201).json(tweet);
 });
 
@@ -63,9 +38,8 @@ router.put("/:id", (req, res, next) => {
     params: { id },
     body: { text },
   } = req;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweetRepository.update(id, text);
   if (tweet) {
-    tweet.text = text;
     res.status(200).json(tweet);
   } else {
     res.status(404).json({ message: `Tweet id(${id}) not found` });
@@ -75,7 +49,7 @@ router.put("/:id", (req, res, next) => {
 // DELETE /tweets/:id
 router.delete("/:id", (req, res, next) => {
   const { id } = req.params;
-  tweets = tweets.filter((tweet) => tweet.id !== id);
+  tweetRepository.remove(id);
   res.sendStatus(204);
 });
 
