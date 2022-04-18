3dce4ecaea24ffba14a28ceb5f9216079df4c55d
nabeomki <beomki69@gmail.com>
2022-04-18 22:58:22 +0900

    chore: token을 주고받을떄 주의사항 추가

--- client/src/network/socket.ts
+++ client/src/network/socket.ts
@@ -3,6 +3,12 @@ import socket from "socket.io-client";
 export default class Socket {
   private io;
   constructor(baseURL: string, getAccessToken: () => string | null) {
+    // token을 주고 받을 때는 query가 아닌 quth를 이용해야 함
+    // query를 이용하면 노출이 되기 때문에 보안에 좋지 못함
+    // const io = socket(baseURL, {
+    //   query: { token: getAccessToken() },
+    // });
+
     this.io = socket(baseURL, {
       auth: (cb) => cb({ token: getAccessToken() }),
     });
--- server/connection/socket.js
+++ server/connection/socket.js
@@ -11,6 +11,10 @@ class Socket {
     });
 
     this.io.use((socket, next) => {
+      // token을 주고 받을 때는 query가 아닌 quth를 이용해야 함
+      // query를 이용하면 노출이 되기 때문에 보안에 좋지 못함
+      // const token = socket.handshake.query && socket.handshake.query.token;
+
       const token = socket.handshake.auth.token;
       if (!token) {
         return next(new Error("Authentication error"));
